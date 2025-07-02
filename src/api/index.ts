aff96256a8d2d81eb9b66b31a2244ae6593ee597
lhelheo <leonardo.oliveira.villalva@hotmail.com>
2025-07-02 00:22:19 -0300

    feat: testing

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import projectRoutes from "./api/routes/projectRoutes";
+import projectRoutes from "./routes/projectRoutes";
 
 const app = express();
 
@@ -8,6 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", projectRoutes);
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message:
+      "Test endpoint is working!",
+  });
+  return;
+});
 
 app.listen(5000, () => {
   console.log(
